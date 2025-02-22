4d9be5b1cefa137ce80e8ec9b33c8f7aaa971e6e
Jp <joaopoliveirapaulino@gmail.com>
2025-02-22 19:17:36 -0300

    primeiro commit

---
+++ src/app/components/PlacaDeVideo/placa-de-video-list/placa-de-video-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placa-de-video-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './placa-de-video-list.component.html',
+  styleUrl: './placa-de-video-list.component.css'
+})
+export class PlacaDeVideoListComponent {
+
+}
